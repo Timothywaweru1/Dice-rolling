ec4fd191b4602dbb30f567c875f92e6b28ba3fdb
Timothy <timothy.waweru1@student.moringaschool.com>
2025-05-24 14:57:13 +0300

    initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Highlights from './components/Highlights'
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Highlights />
+      <Hero />
+    </div>
+  )
+}
+
+export default App
